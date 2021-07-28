0767d28e68c9ee3c163626372f1750970a1c0406
toshi <toshi_j@163.com>
2021-07-28 09:45:37 +0800

    first commit

---
+++ src/main/java/com/toshi/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.toshi.git;
+
+/**
+ * @author toshi
+ * @create 2021-07-28 9:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
